92dfb895ec60063ffc2d5875d79cd3ef196303b3
Ridwan Onikoyi <onikoyiridwan@gmail.com>
2019-12-30 15:02:56 +0100

    ft setup travis ci

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   test: {
     username: process.env.TEST_DB_USERNAME,
-    password: process.env.TEST_DB_PASSWORD,
+    ...(process.env.TEST_DB_PASSWORD && { password: process.env.TEST_DB_PASSWORD }),
     database: process.env.TEST_DB_NAME,
     host: process.env.TEST_DB_HOST,
     dialect: 'postgres',
